8d0001e9392efe6b8c24d35c82248fb29f0ff491
Jason Shands <jason.shands@gmail.com>
2025-03-02 20:06:05 -0600

    Add file logging

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 using LiveSync;
+using Serilog;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.AddSerilog(Log.Logger);
+
 builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 builder.Services.Configure<SyncConfiguration>(builder.Configuration.GetSection("SyncConfiguration"));
 builder.Services.AddTransient<FileSyncService>();
